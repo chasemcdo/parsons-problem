427f68e02c463db1ea0fcbd68751ba923f54e041
Chase McDougall <chasemcdougall@hotmail.com>
2023-08-27 18:28:47 -0400

    Display problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+import { lines } from "./problem"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+      <div className="flex flex-col">
+        {lines.map(line => (
+          <div className="flex flex-row">
+            {line.tokens.map(token => (
+              <div className="bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700 focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+                {
+                  token.type == "input" ? 
+                    <input className="bg-gray-800 text-white border border-gray-600 rounded-lg text-center"></input>
+                    :
+                    token.text
+                }
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
---
+++ src/app/problem.ts
@@ -0,0 +1,86 @@
+export const lines = [
+  {
+    "indentations": 0,
+    "tokens": [
+      {
+        "text": "count",
+        "type": "user-defined-variable"
+      },
+      {
+        "text": " = ",
+        "type": "operator"
+      },
+      {
+        "type": "input"
+      }
+    ]
+  },
+  {
+    "indentations": 0,
+    "tokens": [
+      {
+        "text": "while",
+        "type": "keyword"
+      },
+      {
+        "text": "count",
+        "type": "user-defined-variable"
+      },
+      {
+        "text": " < ",
+        "type": "operator"
+      },
+      {
+        "type": "input"
+      },
+      {
+        "text": ":",
+        "type": "operator"
+      }
+    ]
+  },
+  {
+    "indentations": 1,
+    "tokens": [
+      {
+        "text": "print",
+        "type": "built-in-function"
+      },
+      {
+        "text": "(",
+        "type": "operator"
+      },
+      {
+        "text": ")",
+        "type": "operator"
+      },
+      {
+        "type": "input"
+      }
+    ]
+  },
+  {
+    "indentations": 1,
+    "tokens": [
+      {
+        "text": "count",
+        "type": "user-defined-variable"
+      },
+      {
+        "text": " = ",
+        "type": "operator"
+      },
+      {
+        "text": "count",
+        "type": "user-defined-variable"
+      },
+      {
+        "text": " + ",
+        "type": "operator"
+      },
+      {
+        "type": "input"
+      }
+    ]
+  }
+]
