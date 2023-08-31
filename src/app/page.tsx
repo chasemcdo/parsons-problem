96d158d8ac2b247b2ff70de0bf7e2e31c173f40f
Chase McDougall <chasemcdougall@hotmail.com>
2023-08-31 14:04:01 -0400

    add indentation and cursors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,14 +20,14 @@ export default function Home() {
     const parentDivs = document.querySelectorAll('#parentDiv');
     const solution = `count = N 
 while count < N : 
-print ( N ) 
-count = count + N 
+\\t print ( N ) 
+\\t count = count + N 
 `;
 
     let currentSolution = "";
     parentDivs.forEach(row => {
       row.childNodes.forEach(cell => {
-        if (!(cell.classList.contains("draggable-mirror") || cell.classList.contains("draggable--original"))) {
+        if (!(cell.classList.contains("draggable-mirror") || cell.classList.contains("draggable--original") || cell.classList.contains("solution-ignore"))) {
           currentSolution = currentSolution.concat(cell.textContent || "N", " ")
         }
       })
@@ -42,14 +42,33 @@ count = count + N
     }
   }
 
+  function toggleIndent(line:number) {
+    const indent = document.querySelector(`#indent_${line}`);
+    if (indent?.classList.contains("hidden")) {
+      indent?.classList.remove("hidden");
+      indent?.classList.remove("solution-ignore");
+    } else {
+      indent?.classList.add("hidden");
+      indent?.classList.add("solution-ignore");
+    }
+    checkCorrectness()
+  }
+
   return (
     <main className="flex flex-col min-h-screen items-center p-24 gap-10">
       <h1>Rearrange the following code blocks such that it prints out each number, starting from the initial assignment.</h1>
       <div className="flex flex-col">
         {lines.map((line, index) => (
           <div id="parentDiv" key={`line ${index}`} className="flex flex-row">
+            <button value={index} onClick={() => toggleIndent(index)} id={`indent ${index}`} className="cursor-pointer text-white hover:text-slate-500 px-5 py-2.5 mr-2 mb-2 solution-ignore">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+              </svg>
+            </button>
+            <p id={`indent_${index}`} className="invisible hidden solution-ignore">\t</p>
+
             {line.tokens.map((token, index) => (
-              <p key={`token ${index}`} className="bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700 focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+              <p key={`token ${index}`} className="cursor-pointer bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700 focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
                 {
                   token.type == "input" ?
                     <input className="bg-gray-800 text-white border border-gray-600 rounded-lg text-center"></input>
